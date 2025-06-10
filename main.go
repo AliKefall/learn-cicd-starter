e980f0520a07362a7b2ca8400f0bcae008057489
AliKefall <osman.ali_shen@outlook.com>
2025-06-11 00:01:19 +0300

    final ver

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
 
 	"github.com/bootdotdev/learn-cicd-starter/internal/database"
 
@@ -25,6 +26,10 @@ type apiConfig struct {
 var staticFiles embed.FS
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
+	}
 
 	port := os.Getenv("PORT")
 	if port == "" {
